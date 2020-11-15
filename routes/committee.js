5d37ab36965e492fa3c647b779c0829296b63291
shalevamit9 <shalevamit9@gmail.com>
2020-11-15 16:40:20 +0200

    syntax error fix

--- routes/committee.js
+++ routes/committee.js
@@ -9,7 +9,7 @@ const validation = require('../middleware/validation');
 const router = express.Router();
 
 /* Handles all chairpersons and committees related actions */
-router.get('/committeeParticipants:committeeName', authorization.formatAndSetToken, authorization.verifyToken, committeeController.getAllCommitteeParticipants);
+router.get('/committeeParticipants/:committeeName', authorization.formatAndSetToken, authorization.verifyToken, committeeController.getAllCommitteeParticipants);
 router.post('/committees', authorization.formatAndSetToken, authorization.verifyToken, committeeController.createNewParticipant);
 router.patch('/committees/:committeeName', authorization.formatAndSetToken, authorization.verifyToken, committeeController.updateCommitteeParticipantRole);
 router.delete('/committees/:committeeName', authorization.formatAndSetToken, authorization.verifyToken, committeeController.deleteCommitteeParticipant);
